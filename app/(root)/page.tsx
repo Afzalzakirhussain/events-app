e5dd05d48cdf58354a0978217fc6625efe19caa4
Afzalzakirhussain <afzalzakirhussain@gmail.com>
2024-06-21 01:16:04 +0530

    implemented signUp Page, SignIn Page, Clerk integrated

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/shared/Header";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+      <Button>Hello</Button>
+    </main>
+  );
+}
